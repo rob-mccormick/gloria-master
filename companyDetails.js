0a2a94e0a53a289c2625312aca81d39242facda1
Robert McCormick <rob@idealrole.com>
2019-10-03 10:15:54 +0100

    Updated for snyk

--- companyDetails.js
+++ companyDetails.js
@@ -1,8 +1,8 @@
 // Company specific details to be used by the Bot
 
 const company = {
-    name: 'GoCardless',
-    locations: ['London', 'Paris', 'Munich', 'San Francisco'],
+    name: 'Snyk',
+    locations: ['London', 'Boston', 'Tel Aviv', 'Ottawa'],
     privacyNotice: 'https://www.idealrole.com/privacy',
     categoryOne: [
         'Building the product',

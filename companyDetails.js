89be0b5bfbcf21a6ac0a192fae1da4b3da01f91f
Robert McCormick <rob@idealrole.com>
2019-10-03 10:41:22 +0100

    Updated for Moneybox

--- companyDetails.js
+++ companyDetails.js
@@ -1,8 +1,8 @@
 // Company specific details to be used by the Bot
 
 const company = {
-    name: 'Snyk',
-    locations: ['London', 'Boston', 'Tel Aviv', 'Ottawa'],
+    name: 'Moneybox',
+    locations: ['London'],
     privacyNotice: 'https://www.idealrole.com/privacy',
     categoryOne: [
         'Building the product',

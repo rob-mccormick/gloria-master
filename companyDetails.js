7a7fe8f8758c5c796c3d149f1b6c8866091fd41a
Robert McCormick <rob@idealrole.com>
2019-10-03 09:18:20 +0100

    Updated for Hibob

--- companyDetails.js
+++ companyDetails.js
@@ -1,8 +1,8 @@
 // Company specific details to be used by the Bot
 
 const company = {
-    name: 'LoopMe',
-    locations: ['London', 'New York', 'Dnipro'],
+    name: 'Hibob',
+    locations: ['Tel Aviv', 'London', 'New York'],
     privacyNotice: 'https://www.idealrole.com/privacy',
     categoryOne: [
         'Building the product',
